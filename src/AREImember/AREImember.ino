bd93f482a7aeefdd9139a29852e95a7271472b21
SqxSpaceMan <rizkareza.p@gmail.com>
2021-02-01 13:34:41 +0700

    Adding the RFID function

--- src/AREImember/AREImember.ino
+++ src/AREImember/AREImember.ino
@@ -1,9 +1,11 @@
 
 #include "Disp_cons.h"
 #include "BLE_cons.h"
+#include "RFID_cons.h"
 
 void setup() {
-
+  Serial.begin(9600);
+  RFIDInit();
   BLEInit();
   DisplayInit();
   drawLogoAreimember();
@@ -15,14 +17,28 @@ void loop() {
 
   if (deviceConnected) {
     ClearDisp();
-    if (1)
-    {
-
-      float tempAmbiente = 100;
-
-      sendtonotify("Squanix \n");
-
+    char *Readbuffer = ReadtoTag();
+    if(Readbuffer != NULL){
+      Serial.print(Readbuffer);
+      sendtonotify(Readbuffer);
+      free(Readbuffer);
     }
+    
+
+
+//    for (byte i = 0; i < sizeof(Readbuffer); i++) {
+//      Serial.print(Readbuffer[i]);
+//    }
+
+
+    //    if (1)
+    //    {
+    //
+    //      float tempAmbiente = 100;
+    //
+    //      sendtonotify("Squanix \n");
+    //
+    //    }
   }
   else {
     drawLogoAreimember();
