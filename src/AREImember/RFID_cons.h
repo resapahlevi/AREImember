281643921d806fef97a6d118617e8691d13538ab
SqxSpaceMan <rizkareza.p@gmail.com>
2021-02-01 13:35:43 +0700

    Header to  RFID

--- src/AREImember/RFID_cons.h
+++ src/AREImember/RFID_cons.h
@@ -1,1 +1,18 @@
- 
+
+#ifndef _RFID_CONS_H__
+#define _RFID_CONS_H__
+
+#include <SPI.h>
+#include <MFRC522.h>
+
+
+#define SS_PIN          2
+#define RST_PIN         5
+
+void RFIDInit(void);
+uint8_t WritetoTag(char *stringtotag);
+char * ReadtoTag();
+uint8_t IsNewCardPresent();
+uint8_t PICC_ReadcardSerial();
+
+#endif
