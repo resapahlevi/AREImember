92a4dd0d59cd1fc54c65e79a5033f38166b38eed
SqxSpaceMan <rizkareza.p@gmail.com>
2021-02-11 21:02:16 +0700

    Adding function to sleep the display

--- src/AREImember/AREImember.ino
+++ src/AREImember/AREImember.ino
@@ -3,22 +3,29 @@
 #include "BLE_cons.h"
 #include "RFID_cons.h"
 
+#define LEDON digitalWrite(12,HIGH);
+#define LEDOFF digitalWrite(12,LOW);
+#define TimeToSleep 300;
+
+uint16_t timersleep = 0;
+
 void setup() {
-  Serial.begin(9600);
   RFIDInit();
   BLEInit();
   DisplayInit();
   drawLogoAreimember();
   delay(2000);
+  pinMode(12, OUTPUT);
+  LEDON;
 }
 
 void loop() {
 
   if (deviceConnected) {
+    LEDOFF;
     if (blemode == readtag) {
       char *Readbuffer = ReadtoTag();
       if (Readbuffer != NULL) {
-        Serial.print(Readbuffer);
         sendtonotify(Readbuffer);
         free(Readbuffer);
       }
@@ -35,10 +42,19 @@ void loop() {
   else {
     char *Readbuffer = ReadtoTag();
     if (Readbuffer != NULL) {
-      Serial.print(Readbuffer);
+      LEDON;
       drawFontFaceDemo(Readbuffer);
       free(Readbuffer);
+      timersleep = 0;
     }
+    timersleep++;
   }
-  delay(1000);
+  
+  if (timersleep == 200) {
+    drawFontFaceDemo("Sleep in \n 10s");
+  }
+  if (timersleep >= 300) {
+    LEDOFF;
+  }
+  delay(100);
 }
